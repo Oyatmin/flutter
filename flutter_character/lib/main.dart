3abf0283cfdb29a86de48e9aceff84eba42b17d1
Oyatmin <oyatmin1031@naver.com>
2022-03-02 12:37:37 +0900

    복습

--- flutter_character/lib/main.dart
+++ flutter_character/lib/main.dart
@@ -1,23 +1,26 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
-void main(){
-  runApp(const MyApp());
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: 'Fromis9',
-      home: Grade(),
+    return MaterialApp(
+      title: 'Chracter+Appbar',
+      theme: ThemeData(
+        primarySwatch: Colors.indigo,
+      ),
+      home: const MyPage(),
     );
   }
 }
 
-class Grade extends StatelessWidget {
-  const Grade({Key? key}) : super(key: key);
+class MyPage extends StatelessWidget {
+  const MyPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -27,191 +30,297 @@ class Grade extends StatelessWidget {
         title: const Text('Fromis9',
           style: TextStyle(
             color: Color(0xfffff26f),
+            letterSpacing: 1.0,
           ),
         ),
+        backgroundColor: Colors.indigo[200],
         centerTitle: true,
-        backgroundColor: Colors.indigo[100],
         elevation: 0.0,
-
+        // leading: IconButton(
+        //   icon: const Icon(Icons.menu),
+        //   onPressed: () {
+        //     print('Menu botton is pressed');
+        //   },
+        // ),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.shopping_cart),
+            onPressed: () {
+              print('Shopping cart button is clicked');
+            },
+          ),
+          IconButton(
+            icon: const Icon(Icons.search),
+            onPressed: () {
+              print('Search button is clicked');
+            },
+          ),
+        ],
       ),
+      drawer: Drawer(
+        backgroundColor: Colors.indigo[100],
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            UserAccountsDrawerHeader(
+              currentAccountPicture: const CircleAvatar(
+                backgroundImage: AssetImage('assets/fromis9_pjw.jpg'),
+                backgroundColor: Colors.white,
+              ),
+              otherAccountsPictures: const [
+                CircleAvatar(
+                  backgroundImage: AssetImage('assets/fromis9_pjw1.jpg'),
+                  backgroundColor: Colors.white,
+                ),
+                CircleAvatar(
+                  backgroundImage: AssetImage('assets/fromis9_pjw2.jpg'),
+                  backgroundColor: Colors.white,
+                ),
+              ],
+              accountName: const Text('Pack Jiwon',
+                style: TextStyle(
+                  color: Color(0xfffff26f),
+                ),
+              ),
+              accountEmail: const Text('packjiwon@fromis9.com',
+                style: TextStyle(
+                  color: Color(0xfffff26f),
+                ),
+              ),
+              onDetailsPressed: () {
+                print('Arrow button is clicked');
+              },
+              decoration: BoxDecoration(
+                color: Colors.indigo[300],
+                borderRadius: const BorderRadius.only(
+                  bottomRight: Radius.circular(20.0),
+                  bottomLeft: Radius.circular(20.0),
+                )
+              ),
+            ),
+            ListTile(
+              leading: Icon(
+                Icons.home,
+                color: Colors.grey[850],
+              ),
+              title: const Text('Home'),
+              onTap: () {
+                print('Home button is clicked');
+              },
+              trailing: const Icon(Icons.add),
+            ),
+            ListTile(
+              leading: Icon(
+                Icons.question_answer,
+                color: Colors.grey[850],
+              ),
+              title: const Text('Q&A'),
+              onTap: () {
+                print('Q&A button is clicked');
+              },
+              trailing: const Icon(Icons.add),
+            ),
+            ListTile(
+              leading: Icon(
+                Icons.settings,
+                color: Colors.grey[850],
+              ),
+              title: const Text('setting'),
+              onTap: () {
+                print('Setting button is clicked');
+              },
+              trailing: const Icon(Icons.add),
+            ),
+          ],
+        ),
+      ),
+
       body: SingleChildScrollView(
         scrollDirection: Axis.vertical,
         child: Padding(
           padding: const EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
-          child: Center(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const Center(
-                  child: CircleAvatar(
-                    backgroundImage: AssetImage('assets/fromis9_pjw.jpg'),
-                    radius: 100.0,
-                  ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Center(
+                child: CircleAvatar(
+                  backgroundImage: AssetImage('assets/fromis9_pjw.jpg'),
+                  backgroundColor: Colors.white,
+                  radius: 100.0,
                 ),
-                const Divider(
-                  thickness: 0.5,
-                  endIndent: 0.0,
-                  color: Colors.grey,
-                  height: 60.0,
+              ),
+              const Divider(
+                endIndent: 0.0,
+                thickness: 0.8,
+                height: 60.0,
+                color: Colors.grey,
+              ),
+              const Text('NAME',
+                style: TextStyle(
+                  color: Color(0xfffff26f),
+                  fontSize: 15.0,
+                  letterSpacing: 2.0,
+                  fontWeight: FontWeight.bold,
                 ),
-                const Text('NAME',
+              ),
+              const SizedBox(
+                height: 10.0,
+              ),
+              const Text('Pack Jiwon',
+                style: TextStyle(
+                  color: Color(0xfffff26f),
+                  fontSize: 25.0,
+                  letterSpacing: 2.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(
+                height: 30.0,
+              ),
+              const Text('BORN',
+                style: TextStyle(
+                  color: Color(0xfffff26f),
+                  letterSpacing: 2.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(
+                height: 5.0,
+              ),
+              const Text('March 20, 1998 (age 23)',
+                style: TextStyle(
+                  color: Color(0xfffff26f),
+                  fontSize: 20.0,
+                  letterSpacing: 2.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const Text('Haeundae-gu, Busan, Republic of Korea',
+                style: TextStyle(
+                  color: Color(0xfffff26f),
+                  fontSize: 18.0,
+                  letterSpacing: 2.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(
+                height: 15.0,
+              ),
+              const Text('ASSOCIATED ACTS',
+                style: TextStyle(
+                  color: Color(0xfffff26f),
+                  letterSpacing: 2.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(
+                height: 5.0,
+              ),
+              const Text('Fromis9',
+                style: TextStyle(
+                  color: Color(0xfffff26f),
+                  fontSize: 20.0,
+                  letterSpacing: 2.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(
+                height: 15.0,
+              ),
+              const Text('HEIGHT',
+                style: TextStyle(
+                  color: Color(0xfffff26f),
+                  letterSpacing: 2.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(
+                height: 5.0,
+              ),
+              const Text('158CM',
+                style: TextStyle(
+                  color: Color(0xfffff26f),
+                  fontSize: 20.0,
+                  letterSpacing: 2.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(
+                height: 15.0,
+              ),
+              const Text('WEIGHT',
+                style: TextStyle(
+                  color: Color(0xfffff26f),
+                  letterSpacing: 2.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(
+                height: 5.0,
+              ),
+              const Text('44KG',
+                style: TextStyle(
+                  color: Color(0xfffff26f),
+                  fontSize: 20.0,
+                  letterSpacing: 2.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(
+                height: 50.0,
+              ),
+              const Center(
+                child: Text('PHOTO',
                   style: TextStyle(
                     color: Color(0xfffff26f),
-                    fontSize: 18.0,
-                    fontWeight: FontWeight.bold,
                     letterSpacing: 2.0,
-                  ),
-                ),
-                const SizedBox(
-                  height: 10.0,
-                ),
-                const Text('Pack Jiwon',
-                  style: TextStyle(
-                    color: Color(0xfffff26f),
-                    fontSize: 30.0,
-                    fontWeight: FontWeight.bold,
-                    letterSpacing: 2.0,
-                  ),
-                ),
-                const SizedBox(
-                  height: 30.0,
-                ),
-                const Text('Born',
-                  style: TextStyle(
-                    color: Color(0xfffff26f),
-                    letterSpacing: 2.0,
-                  ),
-                ),
-                const SizedBox(
-                  height: 5.0,
-                ),
-                const Text('March 20, 1998 (age 23)',
-                  style: TextStyle(
-                    color: Color(0xfffff26f),
-                    fontSize: 20.0,
-                    letterSpacing: 2.0,
+                    fontSize: 22.0,
                     fontWeight: FontWeight.bold,
                   ),
                 ),
-                const Text('Haeundae-gu, Busan, Republic of Korea',
-                  style: TextStyle(
-                    color: Color(0xfffff26f),
-                    fontSize: 18.0,
-                    letterSpacing: 2.0,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                const SizedBox(
-                  height: 20.0,
-                ),
-                const Text('ASSOCIATED ACTS',
-                  style: TextStyle(
-                    color: Color(0xffFFF26F),
-                    letterSpacing: 2.0,
-                  ),
-                ),
-                const SizedBox(
-                  height: 5.0,
-                ),
-                const Text('Fromis9',
-                  style: TextStyle(
-                    color: Color(0xffFFF26F),
-                    fontSize: 20.0,
-                    letterSpacing: 2.0,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                const SizedBox(
-                  height: 20.0,
-                ),
-                const Text('HEIGHT',
-                  style: TextStyle(
-                    color: Color(0xffFFF26F),
-                    letterSpacing: 2.0,
-                  ),
-                ),
-                const SizedBox(
-                  height: 5.0,
-                ),
-                const Text('158CM',
-                  style: TextStyle(
-                    color: Color(0xffFFF26F),
-                    fontSize: 20.0,
-                    letterSpacing: 2.0,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                const SizedBox(
-                  height: 20.0,
-                ),
-                const Text('WEIGHT',
-                  style: TextStyle(
-                    color: Color(0xffFFF26F),
-                    letterSpacing: 2.0,
-                  ),
-                ),
-                const SizedBox(
-                  height: 5.0,
-                ),
-                const Text('44KG',
-                  style: TextStyle(
-                    color: Color(0xffFFF26F),
-                    fontSize: 20.0,
-                    letterSpacing: 2.0,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                const SizedBox(
-                  height: 30.0,
-                ),
-                Row(
-                  children: const [
-                     Icon(Icons.check_circle_outline),
-                     SizedBox(
-                       width: 10.0,
-                     ),
-                     Text('Debut  January 24, 2018',
-                      style: TextStyle(
-                        color: Colors.yellow,
-                        fontSize: 16.0,
-                        letterSpacing: 1.0,
-                      ),
-                    ),
-                  ],
-                ),
-                const SizedBox(
-                  height: 20.0,
-                ),
-                ClipRRect(
-                  borderRadius: const BorderRadius.all(Radius.circular(10.0)),
-                  child: Image.asset('assets/fromis9_pjw1.jpg'),
-                ),
-                const SizedBox(
-                  height: 20.0,
-                ),
-                ClipRRect(
-                  borderRadius: BorderRadius.all(Radius.circular(10.0)),
-                  child: Image.asset('assets/fromis9_pjw2.jpg'),
-                ),
-                const SizedBox(
-                  height: 20.0,
-                ),
-                ClipRRect(
-                  borderRadius: BorderRadius.all(Radius.circular(10.0)),
-                  child: Image.asset('assets/fromis9_pjw3.jpg'),
-                ),
-                const SizedBox(
-                  height: 20.0,
-                ),
-                ClipRRect(
-                  borderRadius: BorderRadius.all(Radius.circular(10.0)),
-                  child: Image.asset('assets/fromis9_pjw4.jpg'),
-                ),
-                const SizedBox(
-                  height: 20.0,
-                ),
-              ],
-            ),
+              ),
+              const Divider(
+                endIndent: 0.0,
+                height: 60.0,
+                thickness: 0.8,
+                color: Colors.grey,
+              ),
+              ClipRRect(
+                borderRadius: const BorderRadius.all(Radius.circular(10.0)),
+                child: Image.asset('assets/fromis9_pjw.jpg'),
+              ),
+              const SizedBox(
+                height: 20.0,
+              ),
+              ClipRRect(
+                borderRadius: const BorderRadius.all(Radius.circular(10.0)),
+                child: Image.asset('assets/fromis9_pjw1.jpg'),
+              ),
+              const SizedBox(
+                height: 20.0,
+              ),
+              ClipRRect(
+                borderRadius: const BorderRadius.all(Radius.circular(10.0)),
+                child: Image.asset('assets/fromis9_pjw2.jpg'),
+              ),
+              const SizedBox(
+                height: 20.0,
+              ),
+              ClipRRect(
+                borderRadius: const BorderRadius.all(Radius.circular(10.0)),
+                child: Image.asset('assets/fromis9_pjw3.jpg'),
+              ),
+              const SizedBox(
+                height: 20.0,
+              ),
+              ClipRRect(
+                borderRadius: const BorderRadius.all(Radius.circular(10.0)),
+                child: Image.asset('assets/fromis9_pjw4.jpg'),
+              ),
+              const SizedBox(
+                height: 20.0,
+              ),
+            ],
           ),
         ),
       ),
